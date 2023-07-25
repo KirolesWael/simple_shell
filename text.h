81e351a419d5cd9478fd02fa72accc9d55cd3a7e
shady2532 <shadyrizk2532@gmail.com>
2023-07-26 02:39:02 +0300

    hello there

---
+++ text.h
@@ -0,0 +1,10 @@
+#ifndef TEXT_H
+#define TEXT_H
+#define NULL ((void *)0)
+
+int _strlen(char *msg);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+#endif
