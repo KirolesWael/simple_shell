2c8c0cea7803f378f7a5a2df817bc17be9b25e06
shady2532 <shadyrizk2532@gmail.com>
2023-07-26 02:31:44 +0300

    hello there

--- print.h
+++ print.h
@@ -1,11 +1,10 @@
 #ifndef PRINT_H
 #define PRINT_H
 
-
-int putChar_to_fd(char c, int fd);
-int _putChar(char c);
-int print(char *msg);
-int print_to_fd(char *msg, int fd);
-int print_err(char *msg);
+int printNormal(char *msg);
+int printError(char *msg);
+int putChar(char c);
+int putCharFD(char c, int fd);
+int printFD(char *msg, int fd);
 
 #endif PRINT_H
